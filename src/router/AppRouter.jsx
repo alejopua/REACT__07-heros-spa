5d996c7bed2792dd82b0f81b91c53a2e2cc1c0ff
alejopua <alejopua@gmail.com>
2024-02-26 15:13:08 -0500

    Refactor import statements in AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Navigate ,Routes, Route } from 'react-router-dom'
-import { MarvelPages } from '../heroes/pages/MarvelPages'
-import { DCPages } from '../heroes/pages/DCPages'
-import { LoginPages } from '../auth/pages/LoginPages'
+
+import { DCPages, MarvelPages } from '../heroes'
+import { LoginPages } from '../auth'
 import { Navbar } from '../ui'
 
 export const AppRouter = () => {
