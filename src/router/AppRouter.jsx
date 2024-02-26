3f985a37e8e9728bcef5c03c94cd95d6044af9b4
alejopua <alejopua@gmail.com>
2024-02-26 14:57:56 -0500

    Add Navbar component to AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import { Navigate ,Routes, Route } from 'react-router-dom'
 import { MarvelPages } from '../heroes/pages/MarvelPages'
 import { DCPages } from '../heroes/pages/DCPages'
 import { LoginPages } from '../auth/pages/LoginPages'
+import { Navbar } from '../ui'
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path="marvel" element={<MarvelPages />} />
         <Route path="dc" element={<DCPages />} />
