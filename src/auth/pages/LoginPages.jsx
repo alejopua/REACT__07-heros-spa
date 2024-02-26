b41585f95032ad8f8b716b31cb6ac411e71b0bba
alejopua <alejopua@gmail.com>
2024-02-26 16:52:46 -0500

    Navigate push / replace - useNavigate

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -1,6 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPages = () => {
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+      navigate('/', { replace: true })
+  }
+
   return (
-    <h1>LoginPages</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button 
+        className="btn btn-primary"
+        onClick={onLogout}
+      >
+        Login
+      </button>
+    </div>
   )
 }
