74b9c8f2b9f7a0bf2dfcf571999d3bd9228ca1f3
alejopua <alejopua@gmail.com>
2024-03-18 21:29:17 -0500

    Add tests for AppRouter component

--- test/router/AppRouter.test.jsx
+++ test/router/AppRouter.test.jsx
@@ -1,8 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AuthContext } from "../../src/auth";
+import { AppRouter } from "../../src/router/AppRouter";
 
 describe('Testing in <AppRouter />', () => {
 
   test('should show login if it is not authenticator', () => {
+    const contextValue = {
+      logged: false,
+    }
+
+    render(
+      <MemoryRouter initialEntries={['/marvel']}>
+        <AuthContext.Provider value={ contextValue }>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    )
     
+    expect( screen.getAllByText('Login')).toBeTruthy();
   });
   
+  test('should show marvel component if it is authenticator', () => {
+    const contextValue = {
+      logged: true,
+    }
+
+    render(
+      <MemoryRouter initialEntries={['/marvel']}>
+        <AuthContext.Provider value={ contextValue }>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    )
+    
+    expect( screen.getAllByText('MarvelPages')).toBeTruthy();
+  });
 });
