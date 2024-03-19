c9e67b1bbc7284e4c700cd5fbc7c16598e6cf90a
alejopua <alejopua@gmail.com>
2024-03-18 22:41:06 -0500

    Add test for showing user name in Navbar component

--- test/ui/component/Navbar.test.jsx
+++ test/ui/component/Navbar.test.jsx
@@ -1,8 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../../../src/auth";
+import { Navbar } from "../../../src/ui";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 describe('Name of the group', () => {
+  const contextValue = { 
+    logged: true,
+    user: {
+      name: 'Juan'
+    },
+    logout: jest.fn()
+  }
+
+  beforeEach( () => jest.clearAllMocks() );
   
-  test('should ', () => {
-    
+  test('should show user name', () => {
+
+    render(
+      <AuthContext.Provider value={ contextValue }>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    expect( screen.getByText('Juan')).toBeTruthy();
+
   });
-  
+
 });
