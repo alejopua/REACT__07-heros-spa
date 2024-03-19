c92bdfa73edfd1d007bcf2e474a9e127076a2098
alejopua <alejopua@gmail.com>
2024-03-18 23:27:54 -0500

    Add test for SearchPages component

--- test/heroes/pages/SearchPages.test.jsx
+++ test/heroes/pages/SearchPages.test.jsx
@@ -1,8 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom'
+import { SearchPages } from "../../../src/heroes/pages/SearchPages";
 
 describe('Name of the group', () => {
   
-  test('should ', () => {
+  test('Should show Batman and the input with the querystring value.', () => {
     
+    render(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchPages />
+      </MemoryRouter>
+    )
+
+    const input = screen.getByRole('textbox')
+    expect( input.value ).toBe('batman')
+
+    const imgHero = screen.getByRole('img')
+    expect( imgHero.src ).toContain('/heroes/dc-batman.jpg')
   });
-  
+
 });
