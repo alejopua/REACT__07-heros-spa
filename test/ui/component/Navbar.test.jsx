9f073fdc7032ba9ce85d6c8e94cc7cdca170aee1
alejopua <alejopua@gmail.com>
2024-03-18 23:00:03 -0500

    Add fireEvent and useNavigate to Navbar.test.jsx

--- test/ui/component/Navbar.test.jsx
+++ test/ui/component/Navbar.test.jsx
@@ -1,21 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { AuthContext } from "../../../src/auth";
 import { Navbar } from "../../../src/ui";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter, useNavigate } from "react-router-dom";
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate
+}));
 
 describe('Name of the group', () => {
   const contextValue = { 
     logged: true,
-    user: {
-      name: 'Juan'
-    },
+    user: { name: 'Juan'},
     logout: jest.fn()
   }
 
   beforeEach( () => jest.clearAllMocks() );
   
   test('should show user name', () => {
-
     render(
       <AuthContext.Provider value={ contextValue }>
         <MemoryRouter>
@@ -25,7 +29,22 @@ describe('Name of the group', () => {
     )
 
     expect( screen.getByText('Juan')).toBeTruthy();
+  });
 
+  test('should call the -logout- and -navigate- when the button is clicked.', () => {
+    render(
+      <AuthContext.Provider value={ contextValue }>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    const logoutBtn = screen.getByRole('button');
+    fireEvent.click( logoutBtn );
+
+    expect( contextValue.logout).toHaveBeenCalled();
+    expect( mockedUseNavigate ).toHaveBeenCalledWith('/login', { replace: true });
   });
 
 });
