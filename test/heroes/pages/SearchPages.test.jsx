0b3e015d71a661927368616272109b0a7558c101
alejopua <alejopua@gmail.com>
2024-03-19 00:54:03 -0500

    Add useNavigate mock and update test cases***

--- test/heroes/pages/SearchPages.test.jsx
+++ test/heroes/pages/SearchPages.test.jsx
@@ -1,8 +1,17 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter, useNavigate } from 'react-router-dom'
 import { SearchPages } from "../../../src/heroes/pages/SearchPages";
 
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate
+}));
+
 describe('Name of the group', () => {
+
+  beforeEach( () => jest.clearAllMocks() )
   
   test('Should show Batman and the input with the querystring value.', () => {
     
@@ -27,13 +36,29 @@ describe('Name of the group', () => {
     )
 
     const alertElement = screen.getByLabelText('alertDanger');
-    
+
     expect(alertElement).toBeTruthy();
     expect(alertElement.textContent).toEqual('Hero batman123 not encountered');
 });
 
   test('should call the -navigate- to the new screen.', () => {
-    
+
+    const inputValue = 'batman';
+    render(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchPages />
+      </MemoryRouter>
+    )
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change( input, { target: { name: 'searchText', value: inputValue }})
+
+    const form = screen.getByRole('form')
+    fireEvent.submit( form )
+
+    console.log( mockedUseNavigate.mock.calls );
+    expect( mockedUseNavigate ).toHaveBeenCalledWith(`?q=${ inputValue }`);
+
   });
 
 });
